d7c77e9890e12b27269b84152ec58c06516f39f0
Dmitry Work Mac Pro <dmitry@dmitry-o.co.uk>
2019-05-10 15:28:00 +0100

    Work in progres...

--- dist/app.77c12427.js
+++ dist/app.77c12427.js
@@ -10729,7 +10729,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54238" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64492" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
